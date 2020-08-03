e53bd2026871ce56038b3a996ad777a84d0ba0e0
haydenn <haydenn@jhu.edu>
2020-08-03 19:41:25 -0400

    Updated patient schema

--- Clinovations_Frontend/src/EHR Data Extraction/patientSchema.ts
+++ Clinovations_Frontend/src/EHR Data Extraction/patientSchema.ts
@@ -1,64 +1,58 @@
 //Defines interfaces for types of patient data extracted from the EHR
 
 export interface patientEncounter { 
-    resource: { 
-        id: string,
-        participants: { 
-            invdividual: { 
-                reference: string
-            }
-        }[],
-        period: { 
-            end: string,
-            start: string
-        },
-        type: { 
-            coding: { 
-                code: number, 
-                display: string,
-                system: string
-            }[],
-            text: string
-        }[], 
-        class: { 
-            code: string
+    id: string,
+    participants: { 
+        invdividual: { 
+            reference: string
         }
+    }[],
+    period: { 
+        end: string,
+        start: string
+    },
+    type: { 
+        coding: { 
+            code: number, 
+            display: string,
+            system: string
+        }[],
+        text: string
+    }[], 
+    class: { 
+        code: string
     }
 }
 
 
 export interface patientProcedure { 
-    resource: { 
-        id: string,
-        period: { 
-            end: string,
-            start: string
-        },
-        coding: { 
-            encoding: { 
-                code: number,
-                display: string,
-                system: string
-            }[],
-            text: string
-        }, 
-        EncounterContext: { 
-            reference: string
-        }
+    id: string,
+    period: { 
+        end: string,
+        start: string
+    },
+    coding: { 
+        encoding: { 
+            code: number,
+            display: string,
+            system: string
+        }[],
+        text: string
+    }, 
+    EncounterContext: { 
+        reference: string
     }
 }
 
 export interface patientMedication { 
-    resource: { 
-        id: string, 
-        status: string,
-        coding: { 
-            encoding: { 
-                code: number,
-                display: string,
-                system: string
-            }[],
-            text: string
-        }
+    id: string, 
+    status: string,
+    coding: { 
+        encoding: { 
+            code: number,
+            display: string,
+            system: string
+        }[],
+        text: string
     }
 }

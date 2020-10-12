9ec7fe14d82e608bc4e947aa9109ad982b616245
Yi Hong <yi@yis-imac.lan>
2020-10-11 20:40:00 -0400

    Temporary commit

--- frontend/src/app/services/fhir.service.ts
+++ frontend/src/app/services/fhir.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 import FHIR from 'fhirclient';
 import Client from 'fhirclient/lib/Client';
 
-export type FhirProviders = "Cerner" | "Athena";
+export type FhirProviders = "Cerner" | "AllScripts" | "NextGen";
 
 @Injectable({
     providedIn: 'root'
@@ -25,11 +25,21 @@ export class FhirService {
                 });
                 break;
 
-            case "Athena":
+            case "AllScripts":
                 FHIR.oauth2.authorize({
-                    iss: "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d",
+                    iss: "https://applescm184region.open.allscripts.com/open",
                     scope: "patient/Patient.read patient/Observation.read launch/patient",
-                    clientId: "7903d5f0-a216-4795-9abc-8bf5f0b3cd17",
+                    clientId: "da8dda9a-10c5-4c1e-9b9c-666d72f341a6",
+                    redirectUri: "http://localhost:4200/findPlan?redirected=true",
+                });                
+                break;
+
+            case "NextGen":
+                FHIR.oauth2.authorize({
+                    iss: "https://fhir.nextgen.com/nge/prod/fhir-api-r4/fhir/R4",
+                    scope: "patient/Patient.read patient/Observation.read launch/patient",
+                    clientId: "l72e989d4abb2443bf9dbae5f74a8221c3",
+                    clientSecret: "833e8cd61e8142f399ba0507bbf53d49",
                     redirectUri: "http://localhost:4200/findPlan?redirected=true",
                 });
                 break;

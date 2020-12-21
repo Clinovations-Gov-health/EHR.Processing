f8a86766f022a786f3ad9b31be89fe0327170a5c
April Ding <apr.ding@gmail.com>
2020-12-20 20:14:37 -0800

    apply footer to all pages

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: `
-    <div style="margin-top: 30px; margin-left: 50px"> 
-        <router-outlet></router-outlet>
-    </div>
-    `,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent { }
